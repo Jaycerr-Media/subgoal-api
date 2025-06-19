80063ce819041ba09b63ee8cc0abfb1ba54d7c18
Jaycerr-Media <jake.hurley@outlook.com>
2025-06-19 11:41:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -54,4 +54,4 @@ app.get("/subcount", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`);
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
